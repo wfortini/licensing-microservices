c4dd9b43d4157766320be86abf3621d010ca85f7
wfortini <wpnfortini@gmail.com>
2021-03-24 00:28:42 -0300

    add services, model, controller organization service

---
+++ organization-service/src/main/java/com/wsoft/organization/controllers/OrganizationController.java
@@ -0,0 +1,37 @@
+package com.wsoft.organization.controllers;
+
+import com.wsoft.organization.model.Organization;
+import com.wsoft.organization.services.OrganizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "v1/organizations")
+public class OrganizationController {
+
+    @Autowired
+    private OrganizationService orgService;
+
+
+    @RequestMapping(value="/{organizationId}",method = RequestMethod.GET)
+    public Organization getOrganization(@PathVariable("organizationId") String organizationId) {
+        return orgService.getOrg(organizationId);
+    }
+
+    @RequestMapping(value="/{organizationId}",method = RequestMethod.PUT)
+    public void updateOrganization( @PathVariable("organizationId") String orgId, @RequestBody Organization org) {
+        orgService.updateOrg( org );
+    }
+
+    @RequestMapping(value="/{organizationId}",method = RequestMethod.POST)
+    public void saveOrganization(@RequestBody Organization org) {
+        orgService.saveOrg( org );
+    }
+
+    @RequestMapping(value="/{organizationId}",method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOrganization( @PathVariable("orgId") String orgId,  @RequestBody Organization org) {
+        orgService.deleteOrg( org );
+    }
+}
