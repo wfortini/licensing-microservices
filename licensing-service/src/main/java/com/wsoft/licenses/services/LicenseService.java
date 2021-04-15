76892311744fbe32f5acd3bbb9273c359f446060
wfortini <wpnfortini@gmail.com>
2021-04-15 01:46:23 -0300

    config circuit breaker

--- licensing-service/src/main/java/com/wsoft/licenses/services/LicenseService.java
+++ licensing-service/src/main/java/com/wsoft/licenses/services/LicenseService.java
@@ -1,5 +1,7 @@
 package com.wsoft.licenses.services;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.wsoft.licenses.clients.OrganizationDiscoveryClient;
 import com.wsoft.licenses.clients.OrganizationFeignClient;
 import com.wsoft.licenses.clients.OrganizationRestTemplateClient;
@@ -10,7 +12,9 @@ import com.wsoft.licenses.repository.LicenseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 @Service
@@ -30,6 +34,7 @@ public class LicenseService {
     @Autowired
     OrganizationDiscoveryClient organizationDiscoveryClient;
 
+    @HystrixCommand
     private Organization retrieveOrgInfo(String organizationId, String clientType){
         Organization organization = null;
 
@@ -65,11 +70,22 @@ public class LicenseService {
                 .withComment(config.getExampleProperty());
     }
 
+    /*
+        @HystrixCommand annotation is used to wrapper the getLicenseByOrg() method with a Hystrix circuit breaker
+        execution.isolation.thread.timeoutInMilliseconds property to set the maximum timeout a Hystrix call will
+        wait before failing to be 12 seconds.
 
+     */
+    @HystrixCommand(fallbackMethod = "buildFallbackLicenseList",
+            commandProperties = {@HystrixProperty(
+            name = "execution.isolation.thread.timeoutInMilliseconds",
+            value = "6000"
+    )})
     public List<License> getLicensesByOrg(String organizationId){
+        randomlyRunLong(); // slow
+
         return licenseRepository.findByOrganizationId( organizationId );
     }
-
 
     public void saveLicense(License license){
         license.withId( UUID.randomUUID().toString());
@@ -84,5 +100,33 @@ public class LicenseService {
 
     public void deleteLicense(License license){
         licenseRepository.delete( license );
+    }
+
+    private void randomlyRunLong(){
+        Random rand = new Random();
+
+        int randomNum = rand.nextInt((3 - 1) + 1) + 1;
+        if (randomNum==3) sleep();
+    }
+    private void sleep(){
+        try {
+            Thread.sleep(11000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+         Method fallback
+     */
+    private List<License> buildFallbackLicenseList(String organizationId){
+        List<License> fallbackList = new ArrayList<>();
+        License license = new License()
+                .withId("0000000-00-00000")
+                .withOrganizationId( organizationId )
+                .withProductName("Sorry no licensing information currently available");
+
+        fallbackList.add(license);
+        return fallbackList;
     }
 }
