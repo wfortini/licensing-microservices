145635653d351c491cc06497921268cbfac18fe1
wfortini <wpnfortini@gmail.com>
2021-07-25 20:27:26 -0300

    implement source event

--- organization-service/src/main/java/com/wsoft/organization/services/OrganizationService.java
+++ organization-service/src/main/java/com/wsoft/organization/services/OrganizationService.java
@@ -1,5 +1,6 @@
 package com.wsoft.organization.services;
 
+import com.wsoft.organization.events.source.SimpleSourceBean;
 import com.wsoft.organization.model.Organization;
 import com.wsoft.organization.repository.OrganizationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ public class OrganizationService {
     @Autowired
     private OrganizationRepository orgRepository;
 
+    @Autowired
+    SimpleSourceBean simpleSourceBean;
+
     public Organization getOrg(String organizationId) {
         return orgRepository.findById(organizationId).get();
     }
@@ -21,14 +25,17 @@ public class OrganizationService {
         org.setId( UUID.randomUUID().toString() );
 
         orgRepository.save(org);
+        simpleSourceBean.publishOrgChange("SAVE", org.getId());
 
     }
 
     public void updateOrg(Organization org){
         orgRepository.save(org);
+        simpleSourceBean.publishOrgChange("UPDATE", org.getId());
     }
 
     public void deleteOrg(Organization org){
         orgRepository.delete(org);
+        simpleSourceBean.publishOrgChange("DELETE", org.getId());
     }
 }
